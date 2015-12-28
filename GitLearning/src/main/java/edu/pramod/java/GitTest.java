632b85ca63bf01b3f45ce6b04a05e91c94086e81
Pramod Kumar <pramod.kumar1@ge.com>
2015-12-28 12:10:50 +0530

    Third commit

--- GitLearning/src/main/java/edu/pramod/java/GitTest.java
+++ GitLearning/src/main/java/edu/pramod/java/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("this is to test Git working");
 		System.out.println("Second line");
+		System.out.println("Third line");
 		
 	}
 
